012e1e88d90dbdf836235980ae9531ae1523c350
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-18 17:21:21 -0700

    update splitAreaImages.js

--- public/splitAreaImages.js
+++ public/splitAreaImages.js
@@ -1,4 +1,15 @@
-var idName = `#${document.currentScript.getAttribute('idName')}`
-if (idName === '#') {
-    idName = ''
+let enabled = document.currentScript.getAttribute('enabled')
+if (enabled === 'false' || parseInt(enabled) === 0) enabled = false
+
+if (!!enabled) {
+    let splitAreaImgs = document.querySelectorAll('.split-area > img')
+    splitAreaImgs.forEach(function(img, i) {
+        let imgSrc = img.src
+        if (imgSrc.includes('://')) {
+            imgSrc.imgSrc.split('://')[1]
+        }
+        if (imgSrc.startsWith('via.placeholder.com')) {
+            img.src = `https://via.placeholder.com/${img.clientWidth}x${img.clientHeight}`
+        }
+    })
 }
